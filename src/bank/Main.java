83968325192d8337390e5a19e6cb1d13270b74ed
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-04 11:38:54 -0400

    used decimal format library to format the printed total value

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -2,11 +2,14 @@ package bank;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.text.DecimalFormat;
 
 public class Main {
     public static double total = 0;
 
     public static void main(String[] args) {
+        DecimalFormat fp = new DecimalFormat("$###,###.00");
+
         ArrayList<Currency> piggyBank = new ArrayList<Currency>();
         piggyBank.add(new Quarter());
         piggyBank.add(new Dime());
@@ -16,11 +19,13 @@ public class Main {
         piggyBank.add(new Dollar());
         piggyBank.add(new Penny(10));
 
+        System.out.println("\n***INVENTORY***");
         piggyBank.forEach(c -> {
             total += c.getTotalValue();
             System.out.println(c);
         });
 
-        System.out.println("The piggy bank holds $" + total);
+        System.out.println("\n***TOTAL VALUE***");
+        System.out.println("The piggy bank holds $" + fp.format(total));
     }
 }
