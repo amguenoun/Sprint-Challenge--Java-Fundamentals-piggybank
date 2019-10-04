97a8eae25eb219d385875eaa888ef1c81710c0d8
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-04 11:24:02 -0400

    created arraylist piggyBank and populated it with the currency

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,7 +1,17 @@
 package bank;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Currency> piggyBank = new ArrayList<Currency>();
+        piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
     }
 }
